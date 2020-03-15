d236f5e152cf1b6712155678e02a659e1de37842
lf7777 <939007066@qq.com>
2020-03-15 22:28:23 +0800

    mysql笔记完善

--- python_mysql/select_mould.py
+++ python_mysql/select_mould.py
@@ -1,6 +1,7 @@
 import pymysql
 
-#1. 打开数据库连接    数据库地址 用户名 密码 选择哪个库 设置编码          设置返回的数据格式(默认元祖套元祖,Dict为列表套字典,只有这两种)
+#1. 打开数据库连接    数据库地址 用户名 密码 选择哪个库 设置编码          
+#设置返回的数据格式(默认元祖套元祖,Dict为列表套字典,只有这两种)
 db = pymysql.connect('localhost','root','lf','my_mysql',charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
 #或 单条语句指定数据库
 #db.select_db('my_mysql')
